12c926192e21dac607a587665a9e8da532742037
avazbek27 <avazbekovavazbek277@gmail.com>
2025-07-18 10:07:19 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+// 1. String (строка)
+
+let turshak = "Ismoil";
+console.log(turshak)
+
+
+// 2. Number (число)
+
+let price = 199.99;
+console.log(price)
+
+
+// 3. BigInt Для очень больших чисел:
+
+let bigNumber = 123456789012345678901234567890n;
+console.log(bigNumber);
+
+
+// 4. Boolean Логический тип:
+
+let isAdmin = true;
+let isDelete = false;
+
+
+// 5.underfined Когда переменна обьявлена, но не получила значения:
+
+let test;
+console.log(test);
+
+
+// 6. null Специально "ничего":
+
+let result = null;
+console.log(result);
+
+
+// 7.Symbol Уникальное значение:
+
+let id = Symbol("UserID");
+
+
+// 8.Object Структурированный тип:
+let person = {
+  name: "Abdubosit",
+  age: 30,
+  soqqa: false,
+  oynashi: true,
+  job: 'mashka alfons' 
+}
+
+
+
+console.log(person)
+console.log(person.job)
